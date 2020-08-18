01361333c1032823bea336361c25160660baebe4
Szabó Dániel <szabo.daniel88@gmail.com>
2020-08-18 13:41:55 +0200

    main activity

---
+++ app/src/main/java/com/mordred/grocerylist/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mordred.grocerylist.view;
+
+import com.mordred.grocerylist.R;
+import com.mordred.grocerylist.model.db.ObjectBoxStore;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
