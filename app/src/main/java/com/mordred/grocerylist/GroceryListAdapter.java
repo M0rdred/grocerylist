18fc3ff2658df509ee3e830d240290d71f8ac48d
Szabó Dániel <szabo.daniel88@gmail.com>
2020-08-18 14:29:43 +0200

    grocery list adapter

---
+++ app/src/main/java/com/mordred/grocerylist/GroceryListAdapter.java
@@ -0,0 +1,62 @@
+package com.mordred.grocerylist;
+
+import java.util.List;
+
+import org.apache.commons.collections4.CollectionUtils;
+
+import com.mordred.grocerylist.GroceryListAdapter.GroceryListViewHolder;
+import com.mordred.grocerylist.model.GroceryListEntry;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
+
+public class GroceryListAdapter extends RecyclerView.Adapter<GroceryListViewHolder> {
+
+	private Context context;
+	private List<GroceryListEntry> groceryList;
+
+	public GroceryListAdapter(Context context) {
+		this.context = context;
+	}
+
+	@NonNull
+	@Override
+	public GroceryListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+		LayoutInflater inflater = LayoutInflater.from(this.context);
+		View view = inflater.inflate(R.layout.layout_groceryList, parent, false);
+
+		return new GroceryListViewHolder(view);
+	}
+
+	@Override
+	public void onBindViewHolder(
+			@NonNull GroceryListViewHolder holder, int position
+	) {
+		if (CollectionUtils.isNotEmpty(this.groceryList)) {
+			holder.nameText.setText(this.groceryList.get(position).getName());
+		}
+	}
+
+	@Override
+	public int getItemCount() {
+		return this.groceryList != null ? this.groceryList.size() : 0;
+	}
+
+	public class GroceryListViewHolder extends ViewHolder {
+
+		public TextView nameText;
+
+		public GroceryListViewHolder(@NonNull View itemView) {
+			super(itemView);
+
+			this.nameText = itemView.findViewById(R.id.layoutGroceryList_textViewName);
+		}
+
+	}
+}
--- app/src/main/java/com/mordred/grocerylist/view/MainActivity.java
+++ app/src/main/java/com/mordred/grocerylist/view/MainActivity.java
@@ -1,18 +1,34 @@
 package com.mordred.grocerylist.view;
 
+import com.mordred.grocerylist.GroceryListAdapter;
 import com.mordred.grocerylist.R;
 import com.mordred.grocerylist.model.db.ObjectBoxStore;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
+	private RecyclerView recyclerView;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		this.setContentView(R.layout.activity_main);
 
 		ObjectBoxStore.initStore(this);
+
+		this.initLayout();
+
+	}
+
+	private void initLayout() {
+		this.recyclerView = this.findViewById(R.id.activityMain_recyclerView);
+		this.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+		GroceryListAdapter groceryListAdapter = new GroceryListAdapter(this);
+		this.recyclerView.setAdapter(groceryListAdapter);
 	}
 }
